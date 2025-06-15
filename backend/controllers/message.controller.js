106efaf836f744e6686b022615bdf25a66688904
Hritik Jaiswal <fakehrj@gmail.com>
2025-06-15 20:56:24 +0530

    feat: add message sending functionality with conversation handling

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,40 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import Conversation from "../models/conversation.model.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import {ApiError} from "../utils/ApiError.js";
+import messageModel from "../models/message.model.js";
+import Message from "../models/message.model.js";
+
+
+const sendMessage = asyncHandler(async (req, res) => {
+    const senderId = req.id;
+    const recieverId = req.params.id;
+    const {message} = req.body;
+    let gotConversation = await Conversation.findOne({
+        participants:{$all:[senderId, recieverId]}
+    })
+
+    if(!gotConversation){
+        gotConversation = await Conversation.create({
+            participants:[senderId, recieverId]
+        })
+    }
+
+    const newMessage = await Message.create({
+    senderId,
+    recieverId, 
+    message
+    })
+    if(newMessage){
+        gotConversation.messages.push(newMessage._id);
+        await gotConversation.save();
+    } else {
+        throw new ApiError(500, "Error sending message");
+    }
+    return res.status(200).json(new ApiResponse(200, "Message sent successfully", {
+        conversationId: gotConversation._id,
+        message: newMessage
+    }));
+})
+
+export { sendMessage };
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
+import messageRoute from "./routes/message.route.js";
 dotenv.config({ path: "./.env" });
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 //routes
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
 app.listen(PORT, () => {
     connectDB();
