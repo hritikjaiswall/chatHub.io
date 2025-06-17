23198bc61d64ff601ac07225115ebc3679839473
Hritik Jaiswal <fakehrj@gmail.com>
2025-06-17 16:29:01 +0530

    feat: add SideBar and MessageContainer components, and include react-icons dependency

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import SideBar from './sideBar.jsx'
+import MessageContainer from './MessageContainer.jsx'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    
+    // <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-blue-950 bg-clip-padding backdrop:filter backdrop-blur-lg bg-opacity-10">
+    <div className = "flex sm:h-[450px] md:h-[550px] rounded-lg bg-blue-400  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
+      <SideBar />
+      <MessageContainer /> 
+    </div>
   )
 }
 
---
+++ frontend/src/components/sideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { MdOutlinePersonSearch } from "react-icons/md";
+
+
+
+function SideBar() {
+  return (
+    <div className='border-r border-slate-800 p-4 flex flex-col'>
+      <form action ="" className='flex items-center gap-2 p-2'>
+        <input type="text" placeholder="Search..." className="input input-bordered rounded-md text-gray-600 font-bold bg-[#7989a3] placeholder:text-gray-600" />
+        <button type="submit" className="btn btn-rounded bg-[#7989a3] border border-[#7989a3]">
+          <MdOutlinePersonSearch className ="w-6 h-6 "/>
+        </button>
+      </form>
+      <div className='divider px-2'> </div>
+    </div>
+  )
+}
+
+export default SideBar
